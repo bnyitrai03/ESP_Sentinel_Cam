7c47f14a8c2e789ccd5257770f8f238228dc5b8b
leventenyiri <nyiri.levi88@gmail.com>
2025-02-05 12:33:36 +0100

    added documentation tools

---
+++ docs/conf_common.py
@@ -0,0 +1,30 @@
+from esp_docs.conf_docs import *  # noqa: F403,F401
+
+extensions += [
+    'sphinx_copybutton',
+    'esp_docs.esp_extensions.dummy_build_system',
+    'esp_docs.esp_extensions.run_doxygen',
+]
+
+# Repository and project settings
+github_user = 'bnyitrai03'
+github_repo = 'ESP_Sentinel_Cam'
+
+# HTML context for theme and GitHub integration
+html_context['github_user'] = github_user
+html_context['github_repo'] = github_repo
+html_context['github_version'] = 'main'
+
+# Project information
+project = github_repo
+author = 'Levente Nyiri, Bence Nyitrai'
+copyright = '2024, ' + author
+
+# Documentation settings
+languages = ['en']
+language = 'en'
+todo_include_todos = True
+html_static_path = []
+
+# Theme settings
+html_baseurl = f'https://leventenyiri.github.io/{github_repo}/'
